5b058e0087ade4b6f31ceac4be5d309da7e1f738
jack <jackdek11@gmail.com>
2023-05-31 19:42:14 +0200

    Added PictureDetails component

---
+++ metmuse/src/components/PictureDetails.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ picture }}
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'picture-detail',
+
+  props: {
+    picture: {
+      type: Object,
+      default: null,
+    },
+  }
+}
+
+</script>
