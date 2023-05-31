bf20d58a54c6079bef545295c3788bccfbe67679
jack <jackdek11@gmail.com>
2023-05-31 18:32:14 +0200

    Added photosMixin class to handle calls to the BE

---
+++ metmuse/src/mixins/calls.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+
+export default class photosMixin {
+    static async getPhotos(page = 1) {
+        try{
+            let response = await axios.get(
+                `http://127.0.0.1:8000/api/v1/images?page=${page}`,
+            );
+            return response;
+        } catch (e){
+            console.log("oop");
+            return {}
+        }
+        
+    }
+    static searchPhoto(id) {
+        return axios.get(`http://127.0.0.1:8000/${id}/`);
+    }
+}
