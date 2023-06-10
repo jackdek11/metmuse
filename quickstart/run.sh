b522084719c30c5b4bf1ea93f3d8cb3871ac8ca1
jack <jackdek11@gmail.com>
2023-06-10 12:53:16 +0200

    Refined run.sh script

--- quickstart/run.sh
+++ quickstart/run.sh
@@ -1,2 +1,32 @@
-docker-compose down --rem
-docker-compose up --build
+sigterm_handler() { 
+    echo "here"
+    tput reset
+    docker compose down --remove-orphans
+    exit 1
+}
+
+## Setup signal trap
+trap 'trap " " SIGINT SIGTERM SIGHUP; kill 0; wait; sigterm_handler' SIGINT SIGTERM SIGHUP
+
+docker compose down --remove-orphans
+{
+    docker compose up --build -d 
+    tput reset
+    BLUE='\033[0;34m'
+    NC='\033[0m'
+    echo ""
+    echo ""
+    echo ""
+    echo "     Services are up!"
+    echo "     To visit the website, please navigate to:"
+
+    echo ""
+    echo ""
+    printf "          * ${BLUE}$(tput bold)http://localhost:8080${NC}\n"
+    printf "          * ${BLUE}$(tput bold)http://127.0.0.1:8080${NC}"
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    read
+} 
