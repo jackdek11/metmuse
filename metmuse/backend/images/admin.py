7b90ac3cd74cdcf1b451b6ec0ae76efcf8655ab0
jack <jackdek11@gmail.com>
2023-06-10 13:06:33 +0200

    registrered Queues in admin panel

--- metmuse/backend/images/admin.py
+++ metmuse/backend/images/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django_rq.admin import QueueAdmin
+from django_rq.models import Queue
 
 from images.models import Image
 
 
 admin.site.register(Image)
+admin.site.register(Queue, QueueAdmin)
