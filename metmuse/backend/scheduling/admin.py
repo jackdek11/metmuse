bcb8fbcc02c119362c24eb0286541dce51054adc
jack <jackdek11@gmail.com>
2023-06-11 18:39:02 +0200

    registered scheduling models in admin panel

--- metmuse/backend/scheduling/admin.py
+++ metmuse/backend/scheduling/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from django_rq.admin import QueueAdmin
 from django_rq.models import Queue
 
+from scheduling.models import Department, FetchableImages
+
 
 admin.site.register(Queue, QueueAdmin)
+admin.site.register(Department)
+admin.site.register(FetchableImages)
