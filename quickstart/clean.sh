854f6411bb8da2de2b358ebe1b0199be8f73babe
jack <jackdek11@gmail.com>
2023-06-11 13:10:22 +0200

    added clean.sh script

---
+++ quickstart/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# **************************************************************
+# Authored by Jack de Kock, https://github.com/jackdek11
+# 11th June 2023
+# **************************************************************
+#
+
+docker-compose down --remove-orphans --rmi local --volumes
