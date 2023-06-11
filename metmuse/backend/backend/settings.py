94794491fb3b5ff2c4d36e0513472c71f1207c0d
jack <jackdek11@gmail.com>
2023-06-11 12:37:06 +0200

    Made redis config for 'requirepass' a TODO section

--- metmuse/backend/backend/settings.py
+++ metmuse/backend/backend/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
 }
 
 # Redis and RQ workers
-REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
+REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', 'nom' * 20)
 
