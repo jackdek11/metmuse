da48cf6ce13d26b73e4e2505773867fee4004fe3
jack <jackdek11@gmail.com>
2023-06-11 13:10:45 +0200

    Added 'services did not bring up' output in run.sh

--- quickstart/run.sh
+++ quickstart/run.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+#
+# **************************************************************
+# Authored by Jack de Kock, https://github.com/jackdek11
+# 2nd June 2023
+# **************************************************************
+#
+
 sigterm_handler() { 
     tput reset
     docker compose down --remove-orphans
@@ -29,4 +37,16 @@ docker compose down --remove-orphans
     echo ""
     echo ""
     read
+} || {
+    tput reset
+    BLUE='\033[0;34m'
+    NC='\033[0m'
+    echo ""
+    echo ""
+    echo "     The was an issue bring up the services."
+    echo "     Please ensure that you have follow the setup guide for running the project"
+    echo ""
+    printf "          * ${BLUE}$(tput bold)https://github.com/jackdek11/metmuse/blob/main/README.md${NC}\n"
+    echo ""
+    echo ""
 }
