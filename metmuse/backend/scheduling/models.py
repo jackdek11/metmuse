24368c6e4e6d428c0d356c7fff53b48c9dce1a6e
jack <jackdek11@gmail.com>
2023-06-11 18:35:09 +0200

    Added FetchableImages model and generated new migrations

--- metmuse/backend/scheduling/models.py
+++ metmuse/backend/scheduling/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from scheduling.enums import FetchStatus
 
 
 class Department(models.Model):
@@ -9,3 +10,9 @@ class Department(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class FetchableImages(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    object_id = models.IntegerField()
+    status = models.IntegerField(choices=FetchStatus.choices, default=FetchStatus.NEEDS_FETCHING)
