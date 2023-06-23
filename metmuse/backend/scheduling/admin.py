a1accc933581db16b7115f7ebbf8b70a43ad5573
jack <jackdek11@gmail.com>
2023-06-23 21:25:08 +0200

    Changed FetchableImages admin

--- metmuse/backend/scheduling/admin.py
+++ metmuse/backend/scheduling/admin.py
@@ -4,7 +4,10 @@ from django_rq.models import Queue
 
 from scheduling.models import Department, FetchableImages
 
-
 admin.site.register(Queue, QueueAdmin)
 admin.site.register(Department)
-admin.site.register(FetchableImages)
+
+
+@admin.register(FetchableImages)
+class FetchableImagesAdmin(admin.ModelAdmin):
+    list_display = ('object_id', 'department', 'status')
