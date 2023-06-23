6911a7f69e99b11a6928daf3dbffd33c13ef2505
jack <jackdek11@gmail.com>
2023-06-23 22:07:23 +0200

    Added loaddata (fixtures) to BE startup

--- metmuse/backend/dev_tools/building.sh
+++ metmuse/backend/dev_tools/building.sh
@@ -12,4 +12,5 @@ docker-compose up -d db redis rq-worker rq-scheduler
 cd ../metmuse/backend/
 python3 manage.py migrate
 python3 manage.py add_admin
+python3 manage.py loaddata fixtures/*
 python3 manage.py runserver
