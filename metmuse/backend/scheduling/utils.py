0fd12804b16f096531e38f29aa6abf59323ace9f
jack <jackdek11@gmail.com>
2023-06-23 22:08:35 +0200

    Used transaction.atmoic for find_and_lock_image()

--- metmuse/backend/scheduling/utils.py
+++ metmuse/backend/scheduling/utils.py
@@ -1,5 +1,7 @@
 import typing
 
+from django.db import transaction
+
 from scheduling.models import FetchableImages, FetchStatus
 
 
@@ -9,9 +11,10 @@ def find_and_lock_image() -> typing.Optional[FetchableImages]:
     """
 
     # Use select_for_update() to ensure atomicity. This will lock the db row for operations
-    to_fetch = FetchableImages.objects.select_for_update().filter(status=FetchStatus.NEEDS_FETCHING).first()
+    with transaction.atomic():
+        to_fetch = FetchableImages.objects.select_for_update().filter(status=FetchStatus.NEEDS_FETCHING).first()
 
-    if to_fetch:  # Additional check. Return None if no images from the API left
-        to_fetch.status = FetchStatus.BUSY_FETCHING
-        to_fetch.save(update_fields=['status'])
-        return to_fetch
+        if to_fetch:  # Additional check. Return None if no images from the API left
+            to_fetch.status = FetchStatus.BUSY_FETCHING
+            to_fetch.save(update_fields=['status'])
+            return to_fetch
