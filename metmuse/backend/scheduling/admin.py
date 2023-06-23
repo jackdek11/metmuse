92cca91cef1ae188f3ce236d151c2f470f7d8d45
jack <jackdek11@gmail.com>
2023-06-23 22:08:19 +0200

    Deprecated Department model

--- metmuse/backend/scheduling/admin.py
+++ metmuse/backend/scheduling/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django_rq.admin import QueueAdmin
 from django_rq.models import Queue
 
-from scheduling.models import Department, FetchableImages
+from scheduling.models import FetchableImages
 
 admin.site.register(Queue, QueueAdmin)
-admin.site.register(Department)
 
 
 @admin.register(FetchableImages)
 class FetchableImagesAdmin(admin.ModelAdmin):
-    list_display = ('object_id', 'department', 'status')
+    list_display = ('object_id', 'status')
--- metmuse/backend/scheduling/models.py
+++ metmuse/backend/scheduling/models.py
@@ -2,18 +2,7 @@ from django.db import models
 from scheduling.enums import FetchStatus
 
 
-class Department(models.Model):
-    name = models.CharField(max_length=180)
-
-    class Meta:
-        db_table = 'metmuse.departments'
-
-    def __str__(self):
-        return self.name
-
-
 class FetchableImages(models.Model):
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
     object_id = models.IntegerField()
     status = models.IntegerField(choices=FetchStatus.choices, default=FetchStatus.NEEDS_FETCHING)
 
