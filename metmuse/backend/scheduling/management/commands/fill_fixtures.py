21fe4627683c28748a1e731d2c7f67f8e04b1cc1
jack <jackdek11@gmail.com>
2023-06-23 21:26:21 +0200

    Added logic to FetchableImages to fill_fixtures command

--- metmuse/backend/scheduling/management/commands/fill_fixtures.py
+++ metmuse/backend/scheduling/management/commands/fill_fixtures.py
@@ -3,14 +3,23 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from scheduling.models import Department
+from scheduling.models import Department, FetchableImages
+
+DEPARTMENTS_ENDPOINT = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+OBJECTS_IN_DEPARTMENT_ENDPOINT = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=%s"
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        _max = 2
-        obj = json.loads(
-            requests.get('https://collectionapi.metmuseum.org/public/collection/v1/departments').text)
+        """
+        Run only once to build fixtures
+        """
+        obj = json.loads(requests.get(DEPARTMENTS_ENDPOINT).text)
         for department in obj.get('departments'):
             Department.objects.create(id=department.get('departmentId'), name=department.get('displayName'))
+        for department in Department.objects.all():
+            call = OBJECTS_IN_DEPARTMENT_ENDPOINT % str(department.id)
+            obj = json.loads(requests.get(call).text)
+            for objectId in obj.get('objectIDs'):
+                FetchableImages.objects.create(object_id=objectId, department=department)
