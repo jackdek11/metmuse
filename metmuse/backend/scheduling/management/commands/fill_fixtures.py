980428605ee2170f9b4520506a7725474e481d94
jack <jackdek11@gmail.com>
2023-06-23 22:09:26 +0200

    Refined / simplified fill_fixtures command

--- metmuse/backend/scheduling/management/commands/fill_fixtures.py
+++ metmuse/backend/scheduling/management/commands/fill_fixtures.py
@@ -3,10 +3,9 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from scheduling.models import Department, FetchableImages
+from scheduling.models import FetchableImages
 
-DEPARTMENTS_ENDPOINT = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
-OBJECTS_IN_DEPARTMENT_ENDPOINT = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=%s"
+OBJECTS_ENDPOINT = 'https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&q=""'
 
 
 class Command(BaseCommand):
@@ -15,11 +14,6 @@ class Command(BaseCommand):
         """
         Run only once to build fixtures
         """
-        obj = json.loads(requests.get(DEPARTMENTS_ENDPOINT).text)
-        for department in obj.get('departments'):
-            Department.objects.create(id=department.get('departmentId'), name=department.get('displayName'))
-        for department in Department.objects.all():
-            call = OBJECTS_IN_DEPARTMENT_ENDPOINT % str(department.id)
-            obj = json.loads(requests.get(call).text)
-            for objectId in obj.get('objectIDs'):
-                FetchableImages.objects.create(object_id=objectId, department=department)
+        obj = json.loads(requests.get(OBJECTS_ENDPOINT).text)
+        for objectId in obj.get('objectIDs'):
+            FetchableImages.objects.create(object_id=objectId)
