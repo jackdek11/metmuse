39079e47c803bb341c3f7618b2d491b9bc9e86ae
jack <jackdek11@gmail.com>
2023-06-23 21:26:53 +0200

    Finished logic in find_images rq job, fetch image identified in find_and_lock_image()

--- metmuse/backend/scheduling/tasks.py
+++ metmuse/backend/scheduling/tasks.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import uuid
+import logging
 
 from django.core.files.temp import NamedTemporaryFile
 from django.core import files
@@ -8,28 +9,31 @@ from django_rq.decorators import job
 
 from images.models import Image
 
+from scheduling.models import FetchableImages
+from scheduling.utils import find_and_lock_image
+
+IMAGE_ENDPOINT = "https://collectionapi.metmuseum.org/public/collection/v1/objects/%s"
+
+logger = logging.getLogger('default')
+
 
 @job('image-finder')
 def find_images():
-    _max = 2
-    print("hit")
-    # for _id in range(37, 80):
-    #     if _max < 0:
-    #         break
-    #     obj = json.loads(
-    #         requests.get(f'https://collectionapi.metmuseum.org/public/collection/v1/objects/{_id}').text)
-    #     if obj.get('primaryImage'):
-    #         _max -= 1
-    #         response = requests.get(obj.get('primaryImage'), allow_redirects=True, stream=True)
-    #         file_type = response.headers.get('Content-Type', default='image/jpeg').split('/')[-1].lower().strip()
-    #         image_temp_file = NamedTemporaryFile(delete=True)
-    #         image_temp_file.write(response.content)
-    #         temp_file = files.File(image_temp_file, name=f"{uuid.uuid4()}.{file_type}")
-    #         Image.objects.create(
-    #             file=temp_file,
-    #             ref=int(_id), url=obj.get('primaryImage'), name=obj.get('name'), country=obj.get('country'),
-    #             artistDisplayName=obj.get('artistDisplayName'), artistNationality=obj.get('artistNationality'),
-    #             region=obj.get('region'),
-    #         )
-    #         image_temp_file.flush()
-    #         temp_file.flush()
+    next_image: FetchableImages = find_and_lock_image()
+    if not next_image:
+        logger.warning("No more images available")
+        return
+    obj = json.loads(requests.get(IMAGE_ENDPOINT % next_image.object_id).text)
+    if obj.get('primaryImage'):
+        response = requests.get(obj.get('primaryImage'), allow_redirects=True, stream=True)
+        file_type = response.headers.get('Content-Type', default='image/jpeg').split('/')[-1].lower().strip()
+        image_temp_file = NamedTemporaryFile(delete=True)
+        image_temp_file.write(response.content)
+        temp_file = files.File(image_temp_file, name=f"{uuid.uuid4()}.{file_type}")
+        Image.objects.create(
+            file=temp_file, ref=int(next_image.object_id), url=obj.get('primaryImage'), name=obj.get('name'),
+            country=obj.get('country'), artistDisplayName=obj.get('artistDisplayName'),
+            artistNationality=obj.get('artistNationality'), region=obj.get('region'),
+        )
+        image_temp_file.flush()
+        temp_file.flush()
