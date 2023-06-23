e8832d5715d142491d083dc02a67ec70355b6644
jack <jackdek11@gmail.com>
2023-06-23 22:28:50 +0200

    Rearranged order of FetchStatus items

--- metmuse/backend/scheduling/enums.py
+++ metmuse/backend/scheduling/enums.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class FetchStatus(models.IntegerChoices):
-    NEEDS_FETCHING = 1, _("This image still needs to be fetched")
-    BUSY_FETCHING = 2, _("This image is busy being fetched")
+    BUSY_FETCHING = 1, _("This image is busy being fetched")
+    ERROR = 2, _("There was an error while fetching this image")
     FETCHED = 3, _("This image was fetched correctly")
-    ERROR = 4, _("There was an error while fetching this image")
+    NEEDS_FETCHING = 4, _("This image still needs to be fetched")
