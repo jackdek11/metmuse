64fc39191ef721f5d65008fc6ea7c095569a4be0
jack <jackdek11@gmail.com>
2023-06-25 14:39:02 +0200

    Added sleep statement to building.sh dev util command

--- metmuse/backend/dev_tools/building.sh
+++ metmuse/backend/dev_tools/building.sh
@@ -10,6 +10,7 @@ cd ../../../quickstart/
 docker-compose down --remove-orphans
 docker-compose up -d db redis
 cd ../metmuse/backend/
+sleep 8
 python3 manage.py migrate
 python3 manage.py add_admin
 python3 manage.py loaddata fixtures/*
