867342e8c6804389ece00489082c37a0dbe51921
jack <jackdek11@gmail.com>
2023-06-25 13:00:59 +0200

    Added SchedulingError model

--- metmuse/backend/scheduling/models.py
+++ metmuse/backend/scheduling/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from scheduling.enums import FetchStatus
 
 
-class FetchableImages(models.Model):
+class FetchableImage(models.Model):
     object_id = models.IntegerField()
     status = models.IntegerField(choices=FetchStatus.choices, default=FetchStatus.NEEDS_FETCHING)
 
@@ -12,3 +12,17 @@ class FetchableImages(models.Model):
 
     def __str__(self):
         return str(self.object_id)
+
+
+class SchedulingError(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    fetchable_image = models.ForeignKey(FetchableImage, on_delete=models.CASCADE)
+    retries = models.IntegerField(default=0)
+    request_response = models.JSONField(blank=True)
+
+    class Meta:
+        db_table = 'metmuse.scheduling_errror'
+        verbose_name_plural = "Scheduling Errors"
+
+    def __str__(self):
+        return f"Fetchable Image: {str(self.fetchable_image.object_id)}| retries: {str(self.retries)}"
