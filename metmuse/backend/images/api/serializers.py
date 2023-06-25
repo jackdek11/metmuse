641fdbbbdb173a4bcf4093ddb1ea38f0407713f7
jack <jackdek11@gmail.com>
2023-06-25 19:28:57 +0200

    refactored ImageSerializer

--- metmuse/backend/images/api/serializers.py
+++ metmuse/backend/images/api/serializers.py
@@ -1,10 +1,22 @@
+from django.conf import settings
+
 from rest_framework import serializers
 
 from images.models import Image
 
+CMS_HOST = settings.CMS_HOST
+
 
 class ImageSerializer(serializers.ModelSerializer):
+    file = serializers.SerializerMethodField(method_name="get_file")
 
     class Meta:
         model = Image
-        fields = '__all__'
+        fields = (
+            'file', 'url', 'name', 'artist_name', 'artist_bio', 'artist_nationality', 'country', 'region', 'start_date',
+            'end_date'
+        )
+
+    @staticmethod
+    def get_file(obj):
+        return f"{CMS_HOST}/media/{str(obj.file)}"
