fe45dd6e26d2c8079664ddc3deb963ed988f5151
jack <jackdek11@gmail.com>
2023-06-25 19:28:39 +0200

    Refactored Image fields

--- metmuse/backend/images/models.py
+++ metmuse/backend/images/models.py
@@ -5,16 +5,18 @@ class Image(models.Model):
     file = models.FileField(null=True)
     fetchable_image = models.OneToOneField('scheduling.FetchableImage', on_delete=models.CASCADE, primary_key=True)
     url = models.URLField()
-    name = models.TextField(blank=True, null=True)
-    artistDisplayName = models.TextField(blank=True, null=True)
-    artistDisplayBio = models.TextField(blank=True, null=True)
-    artistNationality = models.TextField(blank=True, null=True)
-    country = models.TextField(blank=True, null=True)
-    region = models.TextField(blank=True, null=True)
+    name = models.CharField(blank=True, max_length=180)
+    artist_name = models.CharField(blank=True, max_length=180)
+    artist_bio = models.CharField(blank=True, max_length=380)
+    artist_nationality = models.CharField(blank=True, max_length=180)
+    country = models.CharField(blank=True, max_length=180)
+    region = models.CharField(blank=True, max_length=180)
+    start_date = models.CharField(blank=True, max_length=4)
+    end_date = models.CharField(blank=True, max_length=4)
 
     class Meta:
         db_table = 'metmuse.images'
 
     def __str__(self):
-        return str(self.name) or "No name found"
+        return self.name or "No name found"
 
