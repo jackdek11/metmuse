b339778e47b9db1da817b3c0703472fa7ee04ad0
jack <jackdek11@gmail.com>
2023-07-06 21:49:41 +0200

    Added resize() util method to resize images

--- metmuse/backend/scheduling/utils.py
+++ metmuse/backend/scheduling/utils.py
@@ -1,6 +1,8 @@
 import typing
 import logging
 
+from PIL import Image
+
 from django.db import transaction
 
 from scheduling.models import FetchableImage, FetchStatus
@@ -21,3 +23,23 @@ def find_and_lock_image() -> typing.Optional[FetchableImage]:
             return to_fetch
         else:
             logging.error("find_and_lock_image() could not find an image")
+
+
+def resize(path: str) -> None:
+    image = Image.open(path)
+    width = image.size[0]
+    height = image.size[1]
+    aspect_ratio = width / height
+    target_width = 1920
+    target_height = 1280
+    if width > target_width or height > target_height:
+        attempted_height = target_width / aspect_ratio
+        target = (round(target_width), round(attempted_height))
+        if attempted_height > target_height:
+            attempted_width = target_height * aspect_ratio
+            target = (round(attempted_width), round(target_height))
+        new_image = image.resize(target)
+        new_image.save(path)
+
+
+
