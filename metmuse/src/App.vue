ef6a0595124e27d199fe064403d5c2b0412a9f67
Jack <jackdek11@gmail.com>
2023-12-02 09:15:26 +0200

    more boilerplate changes

--- metmuse/src/App.vue
+++ metmuse/src/App.vue
@@ -1,34 +1,51 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="app">
-    <StartSession v-if="!started" @sessionStarted="handleSessionStarted" />
-    <Slider v-if="started" />
-  </div>
+      <!-- <StartSession v-if="!started" @sessionStarted="handleSessionStarted" />
+    <Slider v-if="started" /> -->
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <script>
-import Slider from './components/Slider.vue';
-import StartSession from './components/StartSession.vue';
+// import Slider from './components/Slider.vue';
+// import StartSession from './components/StartSession.vue';
 
-export default {
-  name: 'app',
-  components: {
-    Slider,
-    StartSession,
-  },
-  data() {
-    return {
-      started: false,
-    };
-  },
-  methods: {
-    handleSessionStarted() {
-      this.started = true;
-    },
-  },
-};
+// export default {
+//   name: 'app',
+//   components: {
+//     Slider,
+//     StartSession,
+//   },
+//   data() {
+//     return {
+//       started: false,
+//     };
+//   },
+//   methods: {
+//     handleSessionStarted() {
+//       this.started = true;
+//     },
+//   },
+// };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -36,7 +53,65 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-body {
-  margin: 0px;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
