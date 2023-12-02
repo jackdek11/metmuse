c9678e5266fab7e899c2ad84c9b248c070a6b191
Jack <jackdek11@gmail.com>
2023-12-02 08:36:43 +0200

    updated package versioning

--- metmuse/src/mixins/calls.js
+++ metmuse/src/mixins/calls.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
 
-
 export default class photosMixin {
-    static async getPhotos(page = 1) {
-        try{
-            let response = await axios.get(
-                `http://127.0.0.1:8000/api/v1/images?page=${page}`,
-            );
-            return response;
-        } catch (e){
-            return {}
-        }
-        
+  static async getPhotos(page = 1) {
+    try {
+      let response = await axios.get(
+        `http://127.0.0.1:8000/api/v1/images?page=${page}`,
+      );
+      return response;
+    } catch (e) {
+      return {};
     }
-    static searchPhoto(id) {
-        return axios.get(`http://127.0.0.1:8000/${id}/`);
-    }
+  }
+  static searchPhoto(id) {
+    return axios.get(`http://127.0.0.1:8000/${id}/`);
+  }
 }
