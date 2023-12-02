d70d228ad82078caac1743bb93dddd19db6c018b
Jack <jackdek11@gmail.com>
2023-12-02 20:53:43 +0200

    Added BaseHeader component

--- metmuse/src/App.vue
+++ metmuse/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import BaseHeader from './components/BaseHeader.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
+    <BaseHeader></BaseHeader>
   </header>
   <body>
     <RouterView />
---
+++ metmuse/src/components/BaseHeader.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+import Burger from './icons/Burger.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      open: false
+    }
+  },
+  mounted() {
+    this.open
+  },
+  methods: {
+    openMenu() {
+      console.log("Test");
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+    </nav>
+  </div>
+  <Burger @open="open"/>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
